ce26cbf4e9ce5ad20930970c96f5e3e641e09d46
singharaj usai <djjd22938@gmail.com>
2023-10-03 03:51:04 -0400

    obj add props solved

--- 02-Precourse-Part-2/16-Objects-Adding-Properties/index.js
+++ 02-Precourse-Part-2/16-Objects-Adding-Properties/index.js
@@ -2,3 +2,9 @@
 const possibleIterable = [4, 3, 9, 6, 23];
 const divByThree = {};
 // ADD CODE HERE
+
+for (let i = 0; i < possibleIterable.length; i++) {
+    if (possibleIterable[i] % 3 === 0) {
+        divByThree[possibleIterable[i]] = i;
+    }
+}
