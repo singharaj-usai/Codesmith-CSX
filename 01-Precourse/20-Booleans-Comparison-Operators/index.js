64838e6fe018e86a9b21b04753faac59d4fb5f59
singharaj usai <djjd22938@gmail.com>
2023-10-03 02:06:50 -0400

    booleans comparison done

--- 01-Precourse/20-Booleans-Comparison-Operators/index.js
+++ 01-Precourse/20-Booleans-Comparison-Operators/index.js
@@ -3,19 +3,23 @@ const small = 2;
 const large = 5342;
 // ADD CODE BELOW (isSmaller)
 
+const isSmaller = small < large;
 
 // 2.
 const num = 45;
 const string = "45";
 // ADD CODE BELOW (isLooselyEqual and isStrictlyEqual)
 
-
+const isLooselyEqual = num == string;
+const isStrictlyEqual = num === string;
+const isBigger = num > string;
 
 // 3.
 const isTrue = true;
 const isFalse = false;
 // ADD CODE BELOW (isTrueNotFalse)
 
+const isTrueNotFalse = isTrue != isFalse;
 
 // Uncomment these to check your work!
 // console.log('Is 2 < 5342?');
