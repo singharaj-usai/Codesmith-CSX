de07c5e81a58edc9b2b02deb924b33e04c71d8dc
singharaj usai <djjd22938@gmail.com>
2023-10-03 03:09:52 -0400

    while loop cond done

--- 02-Precourse-Part-2/08-While-Loops-Conditional-Expression/index.js
+++ 02-Precourse-Part-2/08-While-Loops-Conditional-Expression/index.js
@@ -1,5 +1,12 @@
 // ADD CODE HERE
 
+let addThis = 0;
+let sum = 0;
+
+while (addThis < 10) {
+    sum += addThis;
+    addThis++;
+}
 
 // Uncomment the line below to check your work!
 // console.log(sum);
