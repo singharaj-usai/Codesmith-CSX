8d72c6a0ac2a9cc10a583cd5684b4d57c1cc15e6
singharaj usai <djjd22938@gmail.com>
2023-10-02 23:42:41 -0400

    strings

---
+++ 01-Precourse/08-Strings/index.js
@@ -0,0 +1,14 @@
+// Uncomment the lines below to see examples of strings
+// console.log('A string can be written inside of single quotes.');
+// console.log("It can also be written inside of double quotes.");
+// console.log(`Backticks work, too.`);
+
+// ADD CODE BELOW
+
+
+
+
+// Uncomment the line below to check your work!
+// console.log(mySingleString);
+// console.log(myDoubleString);
+// console.log(myBackString);
