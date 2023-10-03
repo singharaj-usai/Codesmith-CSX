12006e20e9c5a8403c696da4c5c4ec57168633d4
singharaj usai <djjd22938@gmail.com>
2023-10-02 23:58:34 -0400

    template literals string concat

--- 01-Precourse/10-String-Concatenation/index.js
+++ 01-Precourse/10-String-Concatenation/index.js
@@ -1,10 +1,10 @@
 const first = "Welcome";
-const second = " to the ";
+const second = "to the";
 const third = "jungle!";
 
 // ADD CODE BELOW
 
-let welcomeStatement  = first + second + third;
+let welcomeStatement  = `${first} ${second} ${third}`;
 
 // Uncomment the line below to check your work!
 // console.log(welcomeStatement);
