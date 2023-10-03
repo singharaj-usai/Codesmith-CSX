128a653894c59e003de38789cd70ddd6cd3b3e9f
singharaj usai <djjd22938@gmail.com>
2023-10-03 14:47:39 -0400

    droids challenge

---
+++ 03-Functions-and-Execution-Context/16-droids/README.md
@@ -0,0 +1,3 @@
+## Challenge: droids
+
+Complete the function `droids` that accepts an array of strings and iterates through the array using a FOR loop. Update the variable result to `"Found Droids!"` if the array contains the string `"Droids"`. Otherwise update the variable result to `"These are not the droids you're looking for."` Return your updated result.
---
+++ 03-Functions-and-Execution-Context/16-droids/index.js
@@ -0,0 +1,11 @@
+function droids(arr) {
+    let result = '';
+    // ADD CODE HERE
+    return result;
+}
+
+// Uncomment these to check your work!
+// const starWars = ["Luke", "Finn", "Rey", "Kylo", "Droids"]
+// const thrones = ["Jon", "Danny", "Tyrion", "The Mountain", "Cersei"]
+// console.log(droids(starWars)) // should log: "Found Droids!"
+// console.log(droids(thrones)) // should log: "These are not the droids you're looking for."
