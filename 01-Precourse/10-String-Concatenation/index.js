5c0c59a2ac7cceb314972b21d7f971e6b656b74c
singharaj usai <djjd22938@gmail.com>
2023-10-02 23:54:42 -0400

    10 string concat

---
+++ 01-Precourse/10-String-Concatenation/index.js
@@ -0,0 +1,9 @@
+const first = "Welcome";
+const second = "to the";
+const third = "jungle!";
+
+// ADD CODE BELOW
+
+
+// Uncomment the line below to check your work!
+// console.log(welcomeStatement);
