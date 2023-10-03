ceb657fc08aacd12051575aa5e77e373d8c3a497
singharaj usai <djjd22938@gmail.com>
2023-10-03 04:14:19 -0400

    es6 solution for sayHello

--- 03-Functions-and-Execution-Context/03-sayHello/index.js
+++ 03-Functions-and-Execution-Context/03-sayHello/index.js
@@ -1,8 +1,5 @@
 // ADD CODE HERE
-
-function sayHello(name) {
-    return "Hi, " + name;
-}
+const sayHello = (name) => "Hi, " + name;
 
 // Uncomment these to check your work!
 // console.log(sayHello('Mary')); // should log: 'Hi, Mary'
