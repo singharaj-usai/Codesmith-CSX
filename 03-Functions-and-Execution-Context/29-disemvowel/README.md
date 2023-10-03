d263a42df1402cb0c0bdcf96be2dab8ee43511fc
singharaj usai <djjd22938@gmail.com>
2023-10-03 19:48:16 -0400

    chall 29

---
+++ 03-Functions-and-Execution-Context/29-disemvowel/README.md
@@ -0,0 +1,3 @@
+## Challenge: disemvowel
+
+Write a function `disemvowel` that takes in a string and returns a new string with all vowels removed.
---
+++ 03-Functions-and-Execution-Context/29-disemvowel/index.js
@@ -0,0 +1,8 @@
+function disemvowel(string) {
+    // ADD CODE HERE
+}
+
+// Uncomment these to check your work!
+// console.log(disemvowel('CodeSmith')); // => 'CdSmth'
+// console.log(disemvowel('BANANA')); // => 'BNN'
+// console.log(disemvowel('hello world')); // => 'hll wrld'
