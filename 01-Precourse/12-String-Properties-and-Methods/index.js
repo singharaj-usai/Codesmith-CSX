8603ceea5ef0a35dfe71840a5e35387bd8902476
singharaj usai <djjd22938@gmail.com>
2023-10-03 00:11:13 -0400

    string methods done

--- 01-Precourse/12-String-Properties-and-Methods/index.js
+++ 01-Precourse/12-String-Properties-and-Methods/index.js
@@ -3,6 +3,9 @@ let favAlbum = "The Rise and Fall of Ziggy Stardust and the Spiders From Mars"
 
 // ADD CODE BELOW
 
+let count = hometown.length;
+let upper = favAlbum.toUpperCase();
+let lower = favAlbum.toLowerCase();
 
 // Uncomment the line below to check your work!
 // console.log(count);
