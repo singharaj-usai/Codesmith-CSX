4dd3072d601d4f36f59c9aeab8a0f8e8d833a582
singharaj usai <djjd22938@gmail.com>
2023-10-02 23:32:47 -0400

    solved const

--- 01-Precourse/05-Constant-Assignment/index.js
+++ 01-Precourse/05-Constant-Assignment/index.js
@@ -1,11 +1,12 @@
 // Declare a constant myCodingConfidence and assign it a value of "unwavering"
 // ADD CODE HERE
 
+const myCodingConfidence = "unwavering";
 
 // Try un-commenting the line below to test your answer
 // console.log("My confidence in my coding ability is " + myCodingConfidence);
 
 // ADD CODE HERE
 
-
+// myCodingConfidence = "very good";
 
