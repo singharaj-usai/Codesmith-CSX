9cbaa6e3dbebff5d1065a1c00016948b4cb4d85c
singharaj usai <djjd22938@gmail.com>
2023-10-03 03:30:57 -0400

    control flow and iteration

---
+++ 02-Precourse-Part-2/11-Control-Flow-and-Iteration/README.md
@@ -0,0 +1,3 @@
+## Challenge: Control Flow and Iteration
+
+Iterate through the array and multiply a number by 10 if it is greater than or equal to 5.
---
+++ 02-Precourse-Part-2/11-Control-Flow-and-Iteration/index.js
@@ -0,0 +1,6 @@
+const timesTenIfOverFive = [23, 9, 11, 2, 10, 6];
+// ADD CODE HERE
+
+
+// Uncomment the line below to check your work!
+// console.log(timesTenIfOverFive); // -> should print [230, 90, 110, 2, 100, 60]
