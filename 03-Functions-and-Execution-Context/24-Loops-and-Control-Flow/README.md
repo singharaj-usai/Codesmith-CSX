f8d7615a5e1a2472898c3ee8c83d65c2fdbc75d5
singharaj usai <djjd22938@gmail.com>
2023-10-03 15:56:46 -0400

    24

---
+++ 03-Functions-and-Execution-Context/24-Loops-and-Control-Flow/README.md
@@ -0,0 +1,3 @@
+## Challenge: Loops & Control Flow
+
+Write a function `mergingTripletsAndQuints` which takes in two arrays as arguments. This function will return a new array replacing the elements in `array1` if they are divisible by 3 or 5. The number should be replaced with the sum of itself added to the element at the corresponding index in `array2`.
---
+++ 03-Functions-and-Execution-Context/24-Loops-and-Control-Flow/index.js
@@ -0,0 +1,7 @@
+function mergingTripletsAndQuints(array1, array2) {
+    // ADD CODE HERE
+}
+
+// Uncomment these to check your work!
+// console.log(mergingTripletsAndQuints([1, 2, 3, 4, 5, 15], [1, 3, 6, 7, 8, 9])); // expected log [1, 2, 9, 4, 13, 24]
+// console.log(mergingTripletsAndQuints([1, 1, 3, 9, 5, 15], [1, 2, 3, 4, 5, 6])); // expected log [1, 1, 6, 13, 10, 21]
