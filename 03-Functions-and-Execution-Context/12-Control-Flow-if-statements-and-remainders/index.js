5811c6bc4c427a7f9ad451388e33616737860b58
singharaj usai <djjd22938@gmail.com>
2023-10-03 14:35:31 -0400

    solved 12

--- 03-Functions-and-Execution-Context/12-Control-Flow-if-statements-and-remainders/index.js
+++ 03-Functions-and-Execution-Context/12-Control-Flow-if-statements-and-remainders/index.js
@@ -1,5 +1,13 @@
 // ADD CODE HERE
 
+const iLoveEvenNumbers = (number) => {
+    if (number % 2 === 0) {
+        return 'Oh Yeah Evens!';
+    } else {
+        return 'I am too normal for odd numbers';
+    }
+};
+
 // Uncomment these to check your work!
 // console.log(iLoveEvenNumbers(42)); // expected log 'Oh Yeah Evens!'
 // console.log(iLoveEvenNumbers(17)); // expected log 'I am too normal for odd numbers'
