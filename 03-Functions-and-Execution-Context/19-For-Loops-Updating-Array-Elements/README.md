ade7690cab566403927176166f360cbcbdba72fd
singharaj usai <djjd22938@gmail.com>
2023-10-03 14:56:00 -0400

    for loops updating arr el

---
+++ 03-Functions-and-Execution-Context/19-For-Loops-Updating-Array-Elements/README.md
@@ -0,0 +1,3 @@
+## Challenge: For Loops - Updating Array Elements
+
+Using a FOR loop, write a function `addN` which adds the argument `n` to each number in the array `arr` and returns the updated `arr`.
---
+++ 03-Functions-and-Execution-Context/19-For-Loops-Updating-Array-Elements/index.js
@@ -0,0 +1,7 @@
+function addN(arr, n){
+    // ADD CODE HERE
+}
+
+// Uncomment these to check your work!
+// console.log(addN([1, 2, 3], 3)); // expected log [4, 5, 6]
+// console.log(addN([3, 4, 5], 2)); // expected log [5, 6, 7]
