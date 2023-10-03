e8c38a35bc1097542c881dc8ebb50bfd3d5588b1
singharaj usai <djjd22938@gmail.com>
2023-10-03 01:49:41 -0400

    num exponents remainders done

--- 01-Precourse/16-Numbers-Exponents-and-Remainders/index.js
+++ 01-Precourse/16-Numbers-Exponents-and-Remainders/index.js
@@ -1,8 +1,17 @@
 // ADD CODE BELOW
-
+const fiveSquared = 5 ** 2;
+const eightCubed = 8 ** 3;
+const sevenToTheSixth = 7 ** 6;
+const eightyThreeModSix = 83 % 6;
+const sixtyModNine = 60 % 9;
 
 
 
 
 
 // ADD CONSOLE LOGS BELOW
+console.log(fiveSquared);
+console.log(eightCubed);
+console.log(sevenToTheSixth);
+console.log(eightCubed);
+console.log(sixtyModNine);
