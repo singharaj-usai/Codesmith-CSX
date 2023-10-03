9eed2d60adc99e7bcf48e0603882e0c855e642bf
singharaj usai <djjd22938@gmail.com>
2023-10-03 03:35:36 -0400

    control flow iteration solved

--- 02-Precourse-Part-2/11-Control-Flow-and-Iteration/index.js
+++ 02-Precourse-Part-2/11-Control-Flow-and-Iteration/index.js
@@ -1,6 +1,11 @@
 const timesTenIfOverFive = [23, 9, 11, 2, 10, 6];
 // ADD CODE HERE
 
+for (let i = 0; i < timesTenIfOverFive.length; i++) {
+    if (timesTenIfOverFive[i] >= 5) {
+        timesTenIfOverFive[i] *= 10;
+    }
+}
 
 // Uncomment the line below to check your work!
 // console.log(timesTenIfOverFive); // -> should print [230, 90, 110, 2, 100, 60]
