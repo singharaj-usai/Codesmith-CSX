7472de8dbbc3190a2b674e95d7d8a0aec5517722
singharaj usai <djjd22938@gmail.com>
2023-10-03 15:58:44 -0400

    solved 24

--- 03-Functions-and-Execution-Context/24-Loops-and-Control-Flow/index.js
+++ 03-Functions-and-Execution-Context/24-Loops-and-Control-Flow/index.js
@@ -1,6 +1,15 @@
-function mergingTripletsAndQuints(array1, array2) {
-    // ADD CODE HERE
-}
+const mergingTripletsAndQuints = (array1, array2) => {
+    if (array1.length !== array2.length) {
+        throw Error("Arrays must have the same length");
+    }
+    return array1.map((num1, index) => {
+        if (num1 % 3 === 0 || num1 % 5 === 0) {
+            return num1 + array2[index];
+        } else {
+            return num1;
+        }
+    });
+};
 
 // Uncomment these to check your work!
 // console.log(mergingTripletsAndQuints([1, 2, 3, 4, 5, 15], [1, 3, 6, 7, 8, 9])); // expected log [1, 2, 9, 4, 13, 24]
