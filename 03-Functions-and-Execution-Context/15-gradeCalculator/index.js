e8c10b021df8e0dd2e1c0a6a604108542113eeb3
singharaj usai <djjd22938@gmail.com>
2023-10-03 14:45:30 -0400

    solved

--- 03-Functions-and-Execution-Context/15-gradeCalculator/index.js
+++ 03-Functions-and-Execution-Context/15-gradeCalculator/index.js
@@ -1,6 +1,16 @@
-function gradeCalculator(grade) {
-    // ADD CODE HERE
-}
+const gradeCalculator = (grade) => {
+    if (grade >= 90) {
+        return "A";
+    } else if (grade >= 80) {
+        return "B";
+    } else if (grade >= 70) {
+        return "C";
+    } else if (grade >= 60) {
+        return "D";
+    } else {
+        return "F";
+    }
+};
 
 // Uncomment the lines below to test your code
 // console.log(gradeCalculator(92)); // => "A"
