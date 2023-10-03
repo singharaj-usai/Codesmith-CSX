c4188214aa59d3b1e3127f66a63262156a15abd5
singharaj usai <djjd22938@gmail.com>
2023-10-03 15:29:25 -0400

    solved

--- 03-Functions-and-Execution-Context/21-For-Loops-Summing-Array-Elements/index.js
+++ 03-Functions-and-Execution-Context/21-For-Loops-Summing-Array-Elements/index.js
@@ -1,6 +1,10 @@
-function getTheSum(arr){
-    // ADD CODE HERE
-}
+const getTheSum = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+};
 
 // Uncomment these to check your work!
 //console.log(getTheSum([3, 6, 9])); // expected log 18
