bde56cfd91a6682bfdbdd25f42f5b30c8ea9345c
singharaj usai <djjd22938@gmail.com>
2023-10-03 15:33:45 -0400

    solved

--- 03-Functions-and-Execution-Context/22-multiplyAll/index.js
+++ 03-Functions-and-Execution-Context/22-multiplyAll/index.js
@@ -1,4 +1,16 @@
 // ADD CODE HERE
+const multiplyAll = (...args) => {
+    if (args.length === 0) {
+        return 1;
+    }
+
+    let result = 1;
+    for (const num of args) {
+        result *= num;
+    }
+    return result;
+};
+
 
 // Uncomment these to check your work!
 // console.log(multiplyAll(9, 4, 5, 6, 7, 2, 1, 8, 3)) // should log: 362880
