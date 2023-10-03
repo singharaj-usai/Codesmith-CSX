b4d60f74c2bf77b0da603881c939a3204693b87c
singharaj usai <djjd22938@gmail.com>
2023-10-03 15:02:57 -0400

    makeplural challenge

---
+++ 03-Functions-and-Execution-Context/20-makePlural/README.md
@@ -0,0 +1,3 @@
+## Challenge: makePlural
+
+Create a function `makePlural` that accepts an array argument. `makePlural` will iterate through the array, add an 's' to the end of each array element, and return the mutated array.
---
+++ 03-Functions-and-Execution-Context/20-makePlural/index.js
@@ -0,0 +1,11 @@
+function makePlural(arr){
+    // ADD CODE HERE
+}
+
+
+const arr1 = ['lion', 'tiger', 'bear']
+const arr2 = ['computer', 'video game', 'system']
+
+// Uncomment these to check your work!
+// console.log(makePlural(arr1)); // should log: ['lions', 'tigers', 'bears']
+// console.log(makePlural(arr2)); // should log: ['computers', 'video games', 'systems']
