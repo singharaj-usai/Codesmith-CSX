20a1c78e306c47e9af29690daa9dd570c5ac6893
singharaj usai <djjd22938@gmail.com>
2023-10-03 14:43:43 -0400

    gradecalc

---
+++ 03-Functions-and-Execution-Context/15-gradeCalculator/README.md
@@ -0,0 +1,13 @@
+## Challenge: gradeCalculator
+
+Create a function `gradeCalculator` which takes a grade (number) and returns its letter grade.
+
+grades 90 and above should return "A"
+
+grades 80 to 89 should return "B"
+
+grades 70 to 79 should return "C"
+
+grades 60 to 69 should return "D"
+
+59 and below should return "F"
---
+++ 03-Functions-and-Execution-Context/15-gradeCalculator/index.js
@@ -0,0 +1,10 @@
+function gradeCalculator(grade) {
+    // ADD CODE HERE
+}
+
+// Uncomment the lines below to test your code
+// console.log(gradeCalculator(92)); // => "A"
+// console.log(gradeCalculator(84)); // => "B"
+// console.log(gradeCalculator(70)); // => "C"
+// console.log(gradeCalculator(61)); // => "D"
+// console.log(gradeCalculator(43)); // => "F"
