932f9ebde571ddcce545039e07442e7a9dfed631
singharaj usai <djjd22938@gmail.com>
2023-10-03 13:35:40 -0400

    added 07 problem

---
+++ 03-Functions-and-Execution-Context/07-Adding-Arguments/README.md
@@ -0,0 +1,3 @@
+## Challenge: Functions - Adding Arguments
+
+Examine the code given to you. Determine what arguments must be passed into `isX` and `isY` respectively to obtain an output of true for both, and pass in the arguments accordingly.
---
+++ 03-Functions-and-Execution-Context/07-Adding-Arguments/index.js
@@ -0,0 +1,17 @@
+const x = 23
+
+function isX(num) {
+    const x = 44;
+    return num === x;
+}
+
+function isY(num) {
+    return num ===x;
+}
+
+const one = isX(/* ADD CODE HERE */);
+const two = isY(/* ADD CODE HERE */);
+
+// Uncomment these to check your work!
+// console.log(one); // should log: true
+// console.log(two); // should log: true
