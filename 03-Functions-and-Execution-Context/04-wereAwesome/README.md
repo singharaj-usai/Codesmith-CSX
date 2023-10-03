d4331c4b9247dd66be92c529251befec0dbe0488
singharaj usai <djjd22938@gmail.com>
2023-10-03 12:46:10 -0400

    04

---
+++ 03-Functions-and-Execution-Context/04-wereAwesome/README.md
@@ -0,0 +1,5 @@
+## Challenge: wereAwesome
+
+Write a function `wereAwesome` that takes you and your pair programming partner's name and returns the string "[your buddy's name] and [you] are awesome!"
+
+If you are coding on your own, get creative! Instead of a pair programming partner, it could be the name of your friend, your pet, or even your rubber ducky!
---
+++ 03-Functions-and-Execution-Context/04-wereAwesome/index.js
@@ -0,0 +1,8 @@
+const wereAwesome = (you, yourBuddy) => `${yourBuddy} and ${you} are awesome!`;
+
+
+// Uncomment the lines below to test your function:
+// console.log(wereAwesome("Dave", "Will")); // => "Will and Dave are awesome!"
+// console.log(wereAwesome("Victoria", "Jenny")); // => "Jenny and Victoria are awesome!"
+// console.log(wereAwesome("Chris", "Jac")); // => "Jac and Chris are awesome!"
+// console.log(wereAwesome("Phillip", "Skyler")); // => "Skyler and Phillip are awesome!"
