cc13b8a2a114ee072c1b70aff58e0ab7e939ddea
singharaj usai <djjd22938@gmail.com>
2023-10-03 04:05:01 -0400

    addtwo

---
+++ 03-Functions-and-Execution-Context/01-addTwo/README.md
@@ -0,0 +1,3 @@
+## Challenge: addTwo
+
+Create a function `addTwo` that accepts a number, and returns the number plus 2.
---
+++ 03-Functions-and-Execution-Context/01-addTwo/index.js
@@ -0,0 +1,5 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+// console.log(typeof addTwo); // should log: 'function'
+// console.log(addTwo(10)); // should log: 12
