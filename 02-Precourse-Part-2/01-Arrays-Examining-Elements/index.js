2fee1573890a1906c60e021a215015e8e9e8f8f7
singharaj usai <djjd22938@gmail.com>
2023-10-03 02:36:52 -0400

    arrays exam el solved

--- 02-Precourse-Part-2/01-Arrays-Examining-Elements/index.js
+++ 02-Precourse-Part-2/01-Arrays-Examining-Elements/index.js
@@ -2,5 +2,6 @@ const horror = ['Freddy', 'Jason', 'Michael', 'Ghostface', 'Chucky'];
 
 // ADD CODE HERE
 
+const fourthItem = horror[3];
 
 // Write a console.log statement below to check your work!
