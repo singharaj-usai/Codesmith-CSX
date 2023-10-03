49389ace0108472a3e37d6fc0bd452ecc1b7a627
singharaj usai <djjd22938@gmail.com>
2023-10-03 04:09:22 -0400

    03-sayHello

---
+++ 03-Functions-and-Execution-Context/03-sayHello/README.md
@@ -0,0 +1,3 @@
+## Challenge: sayHello
+
+Create a function `sayHello` that accepts a string argument and returns the string 'Hi, ' with the passed-in string added to the end.
---
+++ 03-Functions-and-Execution-Context/03-sayHello/index.js
@@ -0,0 +1,5 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+// console.log(sayHello('Mary')); // should log: 'Hi, Mary'
+// console.log(sayHello('Haley')); // should log: 'Hi, Haley'
