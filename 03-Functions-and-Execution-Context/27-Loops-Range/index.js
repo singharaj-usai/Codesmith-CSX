837e53f320a42bfe1a4951562d6bc730e6f77e5f
singharaj usai <djjd22938@gmail.com>
2023-10-03 19:42:51 -0400

    solved

--- 03-Functions-and-Execution-Context/27-Loops-Range/index.js
+++ 03-Functions-and-Execution-Context/27-Loops-Range/index.js
@@ -1,6 +1,22 @@
-function getTheRange(arr){
-    // ADD CODE HERE
-}
+const getTheRange = (arr) => {
+    if (arr.length === 0) {
+        return [null, null, 0];
+    }
+    let low = arr[0];
+    let high = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < low) {
+            low = arr[i];
+        }
+        if (arr[i] > high) {
+            high = arr[i];
+        }
+    }
+
+    const range = high - low;
+    return [low, high, range];
+};
 
 // Uncomment these to check your work!
 // console.log(getTheRange([3, 2, 5, 4, 7, 9, 10])); // expect log [2, 10, 8]
