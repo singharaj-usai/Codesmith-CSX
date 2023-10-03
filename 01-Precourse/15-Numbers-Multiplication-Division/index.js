a0755585e85b50e8cacdf915078711d2ae13198f
singharaj usai <djjd22938@gmail.com>
2023-10-03 01:43:50 -0400

    solved

--- 01-Precourse/15-Numbers-Multiplication-Division/index.js
+++ 01-Precourse/15-Numbers-Multiplication-Division/index.js
@@ -1,5 +1,10 @@
 // ADD CODE BELOW
 
+const twentyTimesfour = 20 * 4;
+
+const nineOverThree = 9 / 3;
+
+const twoHundredTimesThree = 200.3 * 3;
 
 // Uncomment these to check your work!
 // console.log(twentyTimesFour);
