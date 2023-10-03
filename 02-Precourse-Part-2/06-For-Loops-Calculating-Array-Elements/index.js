cbf11e40a8af881d1b94a050cce416ebe6ae0268
singharaj usai <djjd22938@gmail.com>
2023-10-03 03:04:56 -0400

    for loop calc done

--- 02-Precourse-Part-2/06-For-Loops-Calculating-Array-Elements/index.js
+++ 02-Precourse-Part-2/06-For-Loops-Calculating-Array-Elements/index.js
@@ -1,6 +1,9 @@
 const increaseByTwo = [1, 2, 3, 4, 5];
 // ADD CODE HERE
 
+for (let i = 0; i < increaseByTwo.length; i++) {
+    increaseByTwo[i] += 2;
+}
 
 // Uncomment the line below to check your work!
 // console.log(increaseByTwo); // -> should print [3, 4, 5, 6, 7];
