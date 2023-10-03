c992b607b2c60b3542308b5f2417a66292d895d8
singharaj usai <djjd22938@gmail.com>
2023-10-03 04:06:02 -0400

    addtwo solved

--- 03-Functions-and-Execution-Context/01-addTwo/index.js
+++ 03-Functions-and-Execution-Context/01-addTwo/index.js
@@ -1,5 +1,9 @@
 // ADD CODE HERE
 
+function addTwo(num) {
+    return num + 2;
+}
+
 // Uncomment these to check your work!
 // console.log(typeof addTwo); // should log: 'function'
 // console.log(addTwo(10)); // should log: 12
