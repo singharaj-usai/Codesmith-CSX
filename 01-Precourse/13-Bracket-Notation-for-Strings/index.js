3105cde7c2732d37ea7a4173efd0a192b858e705
singharaj usai <djjd22938@gmail.com>
2023-10-03 00:14:15 -0400

    bracket notation

---
+++ 01-Precourse/13-Bracket-Notation-for-Strings/index.js
@@ -0,0 +1,10 @@
+const songTitle = "Space Oddity"
+const lyrics = "This is Ground Control to Major Tom / You’ve really made the grade"
+
+// ADD CODE BELOW
+
+
+// Uncomment the line below to check your work!
+// console.log(firstLetter);
+// console.log(seventhCharacter);
+// console.log(lastLetter);
