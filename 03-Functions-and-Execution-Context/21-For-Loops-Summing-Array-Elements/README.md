aabcebf608b90d6714fe61632dd4587e62017853
singharaj usai <djjd22938@gmail.com>
2023-10-03 15:28:03 -0400

    summing arr el

---
+++ 03-Functions-and-Execution-Context/21-For-Loops-Summing-Array-Elements/README.md
@@ -0,0 +1,3 @@
+## Challenge: For Loops - Summing Array Elements
+
+Using a FOR loop, write a function `getTheSum` which adds each element in `arr` to find the array total.
---
+++ 03-Functions-and-Execution-Context/21-For-Loops-Summing-Array-Elements/index.js
@@ -0,0 +1,7 @@
+function getTheSum(arr){
+    // ADD CODE HERE
+}
+
+// Uncomment these to check your work!
+//console.log(getTheSum([3, 6, 9])); // expected log 18
+//console.log(getTheSum([10, 11, 12])); // expected log 33
