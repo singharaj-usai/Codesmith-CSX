8ded321746b442aff52823496db58f0d93ce0342
singharaj usai <djjd22938@gmail.com>
2023-10-02 23:38:40 -0400

    prim data types

---
+++ 01-Precourse/07-Primitive-Data-Types/index.js
@@ -0,0 +1,9 @@
+// ADD CODE HERE
+
+
+
+// Uncomment these to check your work!
+// console.log('My name is ' + name);
+// console.log('I am ' + age + ' years old');
+// console.log('It\'s ' + loveJavaScript + ', I love JavaScript');
+// console.log('The list of things I hate about JavaScript is ' + listOfComplaints);
