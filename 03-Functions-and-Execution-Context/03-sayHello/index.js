19b434907079fc8c5bb667cbc116c3808971ef36
singharaj usai <djjd22938@gmail.com>
2023-10-03 04:11:24 -0400

    sayhello solved

--- 03-Functions-and-Execution-Context/03-sayHello/index.js
+++ 03-Functions-and-Execution-Context/03-sayHello/index.js
@@ -1,5 +1,9 @@
 // ADD CODE HERE
 
+function sayHello(name) {
+    return "Hi, " + name;
+}
+
 // Uncomment these to check your work!
 // console.log(sayHello('Mary')); // should log: 'Hi, Mary'
 // console.log(sayHello('Haley')); // should log: 'Hi, Haley'
