e60b8506b1866753eaefa275a876bbc842931ec9
singharaj usai <djjd22938@gmail.com>
2023-10-03 02:11:32 -0400

    22 solved

--- 01-Precourse/22-Typeof-Operator/index.js
+++ 01-Precourse/22-Typeof-Operator/index.js
@@ -3,3 +3,9 @@ const timesSeen = 732;
 const goingToWatchItAgain = true;
 
 // ADD CODE BELOW HERE
+
+const favMovieType = typeof favMovie;
+
+const timesSeenType = typeof timesSeen;
+
+const seeAgainType = typeof goingToWatchItAgain;
