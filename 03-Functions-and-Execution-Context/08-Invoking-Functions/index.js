8e01984251f43117a1cc92efa89b6b021efbca2b
singharaj usai <djjd22938@gmail.com>
2023-10-03 13:38:27 -0400

    08 invoking funcs

---
+++ 03-Functions-and-Execution-Context/08-Invoking-Functions/index.js
@@ -0,0 +1,22 @@
+let calls = "";
+
+function jerry(str) {
+
+}
+
+function george(str) {
+
+}
+
+function elaine(str) {
+
+}
+
+function kramer(str) {
+
+}
+
+
+// Uncomment these to check your work!
+// calls = jerry(calls);
+// console.log(calls); // should log: 'JerryKramerGeorgeElaine'
