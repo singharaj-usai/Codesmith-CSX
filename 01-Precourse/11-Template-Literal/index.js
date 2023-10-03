ffb9b4363d518bef3dca3195e90a0d4545bb862f
singharaj usai <djjd22938@gmail.com>
2023-10-03 00:01:55 -0400

    template literal

---
+++ 01-Precourse/11-Template-Literal/index.js
@@ -0,0 +1,6 @@
+// Declare 2 variables x & y that are Numbers
+// Declare a string 'solution' that when printed to the console reads like:
+// "The sum of <x> and <y> is <x + y>"
+// ADD CODE BELOW
+
+
