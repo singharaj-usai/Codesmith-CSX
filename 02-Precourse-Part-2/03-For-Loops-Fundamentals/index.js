a9ea3364301a34c6414812d578dc1a3f54ce0fca
singharaj usai <djjd22938@gmail.com>
2023-10-03 02:47:36 -0400

    03 for loop solved

--- 02-Precourse-Part-2/03-For-Loops-Fundamentals/index.js
+++ 02-Precourse-Part-2/03-For-Loops-Fundamentals/index.js
@@ -1,6 +1,9 @@
 let countDown = 10;
 // ADD CODE HERE
 
+for (let i = countDown; i > 0; i--) {
+    countDown = i - 1;
+}
 
 // Uncomment the below line to check your work
 // console.log(countDown) // -> should print 0;
