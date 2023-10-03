4e7dcb1123bfe86dd76b438841bb924ef59a747a
singharaj usai <djjd22938@gmail.com>
2023-10-03 14:53:32 -0400

    short circuit

---
+++ 03-Functions-and-Execution-Context/18-Short-Circuiting-a-Loop/README.md
@@ -0,0 +1,3 @@
+## Challenge: Short-Circuiting a Loop
+
+Complete the function `holidays` that accepts an array of strings and iterates through the array. If the array contains the string `"October"`, return `"Happy Halloween"`. Otherwise, return the string `"Have a great day!"`. Do not use a variable to store the result that you are returning.
---
+++ 03-Functions-and-Execution-Context/18-Short-Circuiting-a-Loop/index.js
@@ -0,0 +1,10 @@
+function holidays(arr) {
+    // Do not use a variable to store your result
+    // ADD CODE HERE
+}
+
+// Uncomment these to check your work!
+// const months = ["April", "May", "June", "October"];
+// const animals = ["Cats", "Dogs", "Pigs"];
+// console.log(holidays(months)); // should return: "Happy Halloween"
+// console.log(holidays(animals)); // should return: "Have a great day!"
