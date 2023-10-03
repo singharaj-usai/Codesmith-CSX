8e85288102d0a288d963d7da9c0d893b8b264870
singharaj usai <djjd22938@gmail.com>
2023-10-03 00:19:31 -0400

    bracket notations done

--- 01-Precourse/13-Bracket-Notation-for-Strings/index.js
+++ 01-Precourse/13-Bracket-Notation-for-Strings/index.js
@@ -1,8 +1,11 @@
-const songTitle = "Space Oddity"
-const lyrics = "This is Ground Control to Major Tom / You’ve really made the grade"
+const songTitle = "Space Oddity";
+const lyrics = "This is Ground Control to Major Tom / You’ve really made the grade";
 
 // ADD CODE BELOW
 
+const firstLetter = songTitle[0];
+const seventhCharacter = songTitle[6];
+const lastLetter = lyrics[lyrics.length - 1];
 
 // Uncomment the line below to check your work!
 // console.log(firstLetter);
