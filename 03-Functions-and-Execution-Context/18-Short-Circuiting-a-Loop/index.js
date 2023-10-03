a7b225913fcbb60363767ccd842d41fb0c561ead
singharaj usai <djjd22938@gmail.com>
2023-10-03 14:54:56 -0400

    solved

--- 03-Functions-and-Execution-Context/18-Short-Circuiting-a-Loop/index.js
+++ 03-Functions-and-Execution-Context/18-Short-Circuiting-a-Loop/index.js
@@ -1,8 +1,4 @@
-function holidays(arr) {
-    // Do not use a variable to store your result
-    // ADD CODE HERE
-}
-
+const holidays = (arr) => (arr.includes("October") ? "Happy Halloween" : "Have a great day!");
 // Uncomment these to check your work!
 // const months = ["April", "May", "June", "October"];
 // const animals = ["Cats", "Dogs", "Pigs"];
