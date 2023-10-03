5ee859bd3453c4130934fe230d46d353fae4b22d
singharaj usai <djjd22938@gmail.com>
2023-10-03 02:39:10 -0400

    02 solved

--- 02-Precourse-Part-2/02-Arrays-Adding-Elements/index.js
+++ 02-Precourse-Part-2/02-Arrays-Adding-Elements/index.js
@@ -2,5 +2,7 @@ const  netflixShows = ["Orange is the New Black", "Black Mirror", "Chewing Gum"]
 
 // ADD CODE HERE
 
+const newShow = "Kenobi";
+netflixShows.push(newShow);
 
 // Write a console.log statement below to check your work!
