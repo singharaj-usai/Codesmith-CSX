01502be6217ca7598385bb5f8b0f94f14750c5b5
singharaj usai <djjd22938@gmail.com>
2023-10-03 03:17:24 -0400

    09 solved

--- 02-Precourse-Part-2/09-Control-Flow-If-Statements/index.js
+++ 02-Precourse-Part-2/09-Control-Flow-If-Statements/index.js
@@ -2,5 +2,10 @@ const num = 40;
 let final;
 // ADD CODE HERE
 
+if (num > 100) {
+    final = null;
+} else if (num < 100) {
+    final = num * 2;
+}
 
 // Log final to the console below to check your work
