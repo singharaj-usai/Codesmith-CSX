f8bfde197d4e78bf72d8f2a28db4876d02808314
singharaj usai <djjd22938@gmail.com>
2023-10-03 01:31:02 -0400

    num add sub solved

--- 01-Precourse/14-Numbers-Addition-Subtraction/index.js
+++ 01-Precourse/14-Numbers-Addition-Subtraction/index.js
@@ -1,7 +1,10 @@
 // ADD CODE BELOW
 
+const birthYear = 1999;
+let age = 24;
+let in10Yrs = age + 10
 
 // Uncomment these to check your work!
-// console.log(birthYear);
-// console.log(age);
-// console.log(in10Yrs);
+console.log(birthYear);
+console.log(age);
+console.log(in10Yrs);
