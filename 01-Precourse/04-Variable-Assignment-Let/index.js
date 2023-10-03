039eb1c95ae84bacdf5069103d75b26755da6ede
singharaj usai <djjd22938@gmail.com>
2023-10-02 23:27:56 -0400

    solved 04

--- 01-Precourse/04-Variable-Assignment-Let/index.js
+++ 01-Precourse/04-Variable-Assignment-Let/index.js
@@ -1,12 +1,14 @@
 // Declare a variable favFood and assign it a value of "tacos"
 // ADD CODE HERE
 
+let favFood = "tacos";
 
 // Try un-commenting the line below to test your answer
 // console.log("My favorite food is " + favFood);
 
 // ADD CODE HERE
 
+favFood = "chicken masaman curry";
 
 // Uncomment the line below to check your work!
 // console.log("No, but really, my favorite food is " + favFood);
