83ce72176a5026eaf6475fe333ffad4186e467bb
singharaj usai <djjd22938@gmail.com>
2023-10-03 19:52:08 -0400

    addwaldo chall

---
+++ 03-Functions-and-Execution-Context/30-addWaldo/index.js
@@ -0,0 +1,5 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+// const siliconValley = {'Richard': 'Hendricks', 'Erlich': 'Bachman', 'Bertram': 'Gilfoyle'}
+// console.log(addWaldo(siliconValley)) // should log:{ Richard: 'Hendricks', Erlich: 'Bachman', Bertram: 'Gilfoyle', Waldo: 'unknown' }
