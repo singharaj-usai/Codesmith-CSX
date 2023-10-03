24f8b926838cf8b87a6a7376bd8fc74571c7c149
singharaj usai <djjd22938@gmail.com>
2023-10-03 19:37:55 -0400

    solved 26

--- 03-Functions-and-Execution-Context/26-Control-Flow-if-statements-and-Math-object/index.js
+++ 03-Functions-and-Execution-Context/26-Control-Flow-if-statements-and-Math-object/index.js
@@ -1,8 +1,18 @@
-function closestToTheMark(player1, player2){
-    const theMark = Math.floor(Math.random() * 100)
-    console.log(`If theMark is ${theMark}...`);
-    // ADD CODE HERE
-}
+const closestToTheMark = (player1, player2) => {
+    const theMark = Math.floor(Math.random() * 100);
+    console.log(`If theMark is ${theMark}...`);;
+
+    const diff1 = Math.abs(player1 - theMark);
+    const diff2 = Math.abs(player2 - theMark);
+
+    if (diff1 < diff2) {
+        return "Player 1 is closest";
+    } else if (diff1 < diff2) {
+        return "Player 2 is closest";
+    } else {
+        return "Both players are equally close";
+    }
+};
 
 // Uncomment the line below to check your work!
 // console.log(closestToTheMark(25, 75));
