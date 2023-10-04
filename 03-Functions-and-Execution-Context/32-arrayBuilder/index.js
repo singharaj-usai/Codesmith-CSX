b4e17e8e7b9fe9439de5e43d1ae03b42ac8ac7f4
singharaj usai <djjd22938@gmail.com>
2023-10-03 20:29:41 -0400

    solved arraybuilder

--- 03-Functions-and-Execution-Context/32-arrayBuilder/index.js
+++ 03-Functions-and-Execution-Context/32-arrayBuilder/index.js
@@ -1,5 +1,12 @@
-function arrayBuilder(obj) {
-    // ADD CODE HERE
+const arrayBuilder = (obj) => {
+    const result = []
+    for (const key in obj) {
+        const count = obj[key]
+        for (let i = 0; i < count; i++) {
+            result.push(key)
+        }
+    }
+    return result
 }
 
 // Uncomment these to check your work!
