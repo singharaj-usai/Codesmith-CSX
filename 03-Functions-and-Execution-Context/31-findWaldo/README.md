b9d997091d2839efe26492fa96b96aa6dce21643
singharaj usai <djjd22938@gmail.com>
2023-10-03 20:27:19 -0400

    chall 31

---
+++ 03-Functions-and-Execution-Context/31-findWaldo/README.md
@@ -0,0 +1,3 @@
+## Challenge: findWaldo
+
+Create a function `findWaldo` that accepts an object and returns the value associated with the key `'Waldo'`. If the key `'Waldo'` is not found, the function should return `'Where's Waldo?'`
---
+++ 03-Functions-and-Execution-Context/31-findWaldo/index.js
@@ -0,0 +1,7 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+// const DC = {'Bruce': 'Wayne', 'Harley': 'Quinn'}
+// const supernatural = {'Sam': 'Winchester', 'Dean': 'Winchester', 'Waldo': 'unknown'}
+// console.log(findWaldo(DC)) // should log: 'Where's Waldo?'
+// console.log(findWaldo(supernatural)) // should log: 'unknown'
